ad372c83f55d1490a8be77a62c8226bb4844935b
thejasrao262003 <thejasrao262003@gmail.com>
2025-08-24 23:19:33 +0530

    Wrote first hello world functions, tried some basic syntax

---
+++ Month 1/Week 1/hello/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	i := 1
+	for i<=3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for i<=3 {
+		fmt.Println("range", i)
+	}
+
+	for j:=0;j<=3;j++ {
+		fmt.Println(j)
+	}
+}
