f682f1a4b0f228d9a2d48c1db0cf5ce13a9fbc17
thejasrao262003 <thejasrao262003@gmail.com>
2025-08-24 23:50:58 +0530

    Added error handling

--- Month 1/Week 1/hello/hello.go
+++ Month 1/Week 1/hello/hello.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"example.com/greetings"
 )
 func main(){
-	message := greetings.Hello("Thejas")
-	fmt.Println(message)
+
+	log.SetPrefix("Greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Hello("")
+	if err != nil{
+		log.Fatal(err)
+	}else{
+		fmt.Println(message)
+	}
 }
